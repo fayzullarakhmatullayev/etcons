2fa4446708c04fcf4677937371ea37d627085024
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2023-05-01 01:09:00 +0500

    feature: Многострочный текст в ячейке

--- pages/multi-text/index.vue
+++ pages/multi-text/index.vue
@@ -2,12 +2,55 @@
   <PageBreadcrumb :items="breadcrumb" />
   <div class="container">
     <h2 class="title">Многострочный текст в ячейке</h2>
+    <div class="multi-text">
+      <DataTable :value="data">
+        <Column
+          v-for="col of columns"
+          :key="col.field"
+          :field="col.field"
+          :header="col.header"
+        ></Column>
+      </DataTable>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from "vue";
+interface IText {
+  time: string;
+  text: string;
+}
+import { reactive, ref } from "vue";
+import axios from "axios";
 const breadcrumb = reactive([{ label: "Многострочный текст в ячейке", to: "/multi-text" }]);
+const data = ref<IText[]>([]);
+
+const columns = [
+  { field: "time", header: "Время" },
+  { field: "text", header: "Текст" },
+];
+
+const fetchText = async (): Promise<void> => {
+  try {
+    const response = await axios.get("text.json");
+
+    const text = response.data.text.split("[");
+
+    data.value = text
+      .map((item: string) => {
+        const time = `[${item.slice(0, 8)}]`;
+        const text = item.slice(9).trim();
+        return { time, text };
+      })
+      .filter((item: IText) => item.text !== "" || item.text !== "");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(async () => {
+  await fetchText();
+});
 </script>
 
 <style lang="scss">
--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -2,11 +2,12 @@ import { defineNuxtPlugin } from "#app";
 import locale from "~/locale";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
-import InputMask from "primevue/inputmask";
 import InputText from "primevue/inputtext";
 import Breadcrumb from "primevue/breadcrumb";
 import Calendar from "primevue/calendar";
 import MultiSelect from "primevue/multiselect";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, {
@@ -15,8 +16,9 @@ export default defineNuxtPlugin((nuxtApp) => {
   });
   nuxtApp.vueApp.component("Button", Button);
   nuxtApp.vueApp.component("InputText", InputText);
-  nuxtApp.vueApp.component("InputMask", InputMask);
   nuxtApp.vueApp.component("Breadcrumb", Breadcrumb);
   nuxtApp.vueApp.component("Calendar", Calendar);
   nuxtApp.vueApp.component("MultiSelect", MultiSelect);
+  nuxtApp.vueApp.component("DataTable", DataTable);
+  nuxtApp.vueApp.component("Column", Column);
 });
